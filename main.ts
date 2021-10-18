ac11aba61074e061d21fa1661ad6b2f50a062926
Ruslanus00 <90270667+Ruslanus00@users.noreply.github.com>
2021-10-18 10:14:38 +0300

    Початкові файли для проекту MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+let max = 15
+let min = 2
+let list: number[] = []
+let array: number[] = []
+for (let індекс = 0; індекс <= 4; індекс++) {
+    array.insertAt(індекс, randint(min, max))
+}
+basic.forever(function () {
+	
+})
